454d129e3f434144f75ef8b7827a2fdd18eb6512
ZekKen (Ken) <kenseterra03@gmail.com>
2023-11-12 20:04:11 +0000

    Added basic stat scripts

---
+++ COMP3013 WIP Title/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+
+    public Stat damage;
+    public Stat armor;
+}
