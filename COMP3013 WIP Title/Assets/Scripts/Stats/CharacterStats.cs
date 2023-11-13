3fe683571d80d7f202f292aee179f7d98ab5a8b7
ZekKen (Ken) <kenseterra03@gmail.com>
2023-11-13 01:44:34 +0000

    Updated player stats

--- COMP3013 WIP Title/Assets/Scripts/Stats/CharacterStats.cs
+++ COMP3013 WIP Title/Assets/Scripts/Stats/CharacterStats.cs
@@ -4,7 +4,43 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
 
     public Stat damage;
     public Stat armor;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+        
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        //Die
+        //Method is meant to be overwritten
+        Debug.Log(transform.name + "died");
+    }
 }
