76c9e64cb86e16787b6c35f3b942a6fc73cce5da
Johnl <ionutcriste28@gmail.com>
2023-12-08 12:32:38 +0000

    Card ProtoTable init

--- COMP3013 WIP Title/Assets/Scripts/CardSystem/CardData.cs
+++ COMP3013 WIP Title/Assets/Scripts/CardSystem/CardData.cs
@@ -3,6 +3,7 @@
 public class CardData
 {
   public int id = 0;
+  public int protoId = 0;
   public string name = "";
   public int level = 1;
   public CardElement element = CardElement.NONE;
---
+++ COMP3013 WIP Title/Assets/Scripts/CardSystem/CardProtoManager.cs
@@ -0,0 +1,14 @@
+
+
+public class CardProtoData
+{
+  
+}
+
+public class CardProtoManager
+{
+
+}
+
+
+
