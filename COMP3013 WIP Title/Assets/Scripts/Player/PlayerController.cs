a39704a6e7d255b250a66a4bbb1f4a960150fd22
ZekKen (Ken) <kenseterra03@gmail.com>
2024-01-06 20:22:13 +0000

    Identified rotation issue, fixed. Added new fbx model without camera component

--- COMP3013 WIP Title/Assets/Scripts/Player/PlayerController.cs
+++ COMP3013 WIP Title/Assets/Scripts/Player/PlayerController.cs
@@ -1,22 +1,45 @@
 
+using System;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rigidBody;
+    public float moveSpeed;
+    private Rigidbody rb;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
     private Camera mainCamera;
 
-
-    void Awake()
+    void Start()
     {
-        InitReferences();
-    }
-
-    private void InitReferences()
-    {
-        rigidBody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         mainCamera = FindObjectOfType<Camera>();
     }
 
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+            
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+    }
 }
